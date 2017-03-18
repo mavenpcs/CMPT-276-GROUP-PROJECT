b212dbf3b32d3776d1ff1e77317837904ac8bff6
faffas-tablet <rymei@sfu.ca>
2017-03-17 21:06:31 -0700

    Added activity to see feedback

---
+++ sprint1/src/com/G11/sprint1/SeeFeedback.java
@@ -0,0 +1,15 @@
+package com.G11.sprint1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by faffas-tablet on 2017-03-17.
+ */
+public class SeeFeedback extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_see_feedback);
+    }
+}
