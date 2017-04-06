2986a95ef4a2286e42c194757b74270fea9e3090
Peter <csp7@sfu.ca>
2017-04-05 17:24:16 -0700

    Schedule Activity: 1. Now clears all schedules when there is only 1 schedule in the database. Edit: There was a slight error in the condition for the executing the clear method. Fixed.

--- sprint1/src/com/G11/sprint1/Schedule.java
+++ sprint1/src/com/G11/sprint1/Schedule.java
@@ -47,7 +47,7 @@ public class Schedule extends Activity {
             @Override
             public void onClick(View v) {
                 Cursor c= base.getAllRows();
-                if (c.getColumnCount() > 0) {
+                if (c.getCount() > 0) {
                     base.deleteAll();
                     populate();
                     Toast.makeText(Schedule.this, "All schedule cleared!", Toast.LENGTH_SHORT).show();
